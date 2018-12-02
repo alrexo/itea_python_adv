d9d2cb05fb1953428d01d7fba50f2dab7c1b04a5
Alex Miro <lekskeks@yahoo.de>
2018-12-02 22:01:09 +0200

    lesson_15 added return for get_setting, implemented OS_ENV & Mongo

--- lesson_15/redis_ex.py
+++ lesson_15/redis_ex.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from os import environ
+import re
 
 
 class BaseBackend(ABC):
@@ -16,7 +18,7 @@ class RedisBackend(BaseBackend):
     redis_db = fakeredis.FakeStrictRedis()
 
     def get_setting(self, name):
-        self.redis_db.get(name)
+        return self.redis_db.get(name)
 
     def set_setting(self, name, value):
         self.redis_db.set(name, value)
@@ -26,21 +28,49 @@ class RedisBackend(BaseBackend):
 
 
 class OSENVBackend(BaseBackend):
-    pass
 
+    _BOOL = {'True': True,  'False': False}
 
-class MongoBackend(BaseBackend):
-    from pymongo import MongoClient
-    mongo_db = MongoClient()
+    _regex = re.compile("^[-+]?\d+\.\d+$")
 
     def get_setting(self, name):
-        pass
+        value = environ.get(name, '')
+
+        if value.isdigit():
+            return int(value)
+
+        if OSENVBackend._regex.match(value):
+            return float(value)
+
+        if value in OSENVBackend._BOOL:
+            return OSENVBackend._BOOL[value]
+
+        return value
 
     def set_setting(self, name, value):
-        pass
+        environ[name] = value
 
     def __setattr__(self, name, value):
         self.set_setting(name, value)
+
+
+class MongoBackend(BaseBackend):
+
+    from pymongo import MongoClient
+
+    mongo_db = MongoClient()
+    db = mongo_db.test_database
+    settings = db.settings
+
+    def get_setting(self, name):
+        return self.settings.find_one({'doc_type': 'config'})[name]
+
+    def set_setting(self, name, value):
+        self.settings.update_one({'doc_type': 'config'}, {'$set': {name: value}}, upsert=True)
+
+    def __setattr__(self, name, value):
+        self.set_setting(name, value)
+        print('mongo __setattr__ reached')
 
 
 class Env:
@@ -54,15 +84,23 @@ class Env:
             self.b = MongoBackend()
 
     def __getattr__(self, name):
-        self.b.get_setting(name)
+        return self.b.get_setting(name)
 
     def __setattr__(self, name, value):
         super().__setattr__(name, value)
 
 
-obj = Env()
-print(dir(obj))
-print(dir(obj.b))
+# obj_r = Env()
+# obj_r.setting1 = 'bla redis'
+# print(obj_r.setting1)
+#
+# obj_os = Env('OS_ENV')
+# obj_os.setting1 = 'bla os'
+# print(obj_os.setting1)
+# print('PYTHONPATH:', obj_os.PYTHONPATH)
 
-obj.setting1 = 'bla'
-print(obj.setting1)
+
+obj_mn = Env('Mongo')
+print(obj_mn.config_1)
+obj_mn.setting1 = 'bla mongo'
+print(obj_mn.setting1)
